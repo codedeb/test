7efb1e749de8c9a34c12a94a19494a4e65a0ed4c
Debashish Chakraborty <debashish_c@persistent.co.in>
2020-03-03 21:11:34 +0530

    added new code

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
-def test(*args):
-    return sum(*args)
+def exchangeZero(nums):
+    i = 0
+    j = 0
+    while i < len(nums):
+        if nums[i] != 0:
+            nums[j], nums[i]= nums[i], nums[j]
+            j += 1
 
+        i += 1
+    return nums
 
-obj = test([1, 2, 3, 4, 5, 5])
+obj = exchangeZero([1,0,1,0,1,1,0,1])
 print(obj)
