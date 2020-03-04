8be3bcee26665033dfdce74d36ead6e7fae84737
Debashish Chakraborty <debashish_c@persistent.co.in>
2020-03-04 16:37:11 +0530

    made chanes

--- test.py
+++ test.py
@@ -1,15 +1,9 @@
-def exchangeZero(nums):
-    i = 0
-    j = 0
-    while i < len(nums):
-        if nums[i] != 0:
-            nums[j], nums[i]= nums[i], nums[j]
-            j += 1
+def test(*args):
+    return sum(*args)
 
-        i += 1
-    return nums
 
 
 obj = exchangeZero([1,0,1,0,1,1,0,1])
+
 
 print(obj)
